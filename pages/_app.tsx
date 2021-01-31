73b8cbaa91577786a0591e1d77a5d4b758fa684b
Kit Harvey V. Caubalejo <54163221+kitharvey@users.noreply.github.com>
2021-01-31 23:52:41 +0800

    404 added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,10 @@
 import type { AppProps } from 'next/app'
+import Layout from '../components/Layout'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <Layout title="Home | Next.js + TypeScript Example">
+    <Component {...pageProps} />
+  </Layout>
+  )
 }
--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link'
 
 import { User } from '../../interfaces'
 import { sampleUserData } from '../../utils/sample-data'
-import Layout from '../../components/Layout'
 import List from '../../components/List'
 
 type Props = {
@@ -11,7 +10,7 @@ type Props = {
 }
 
 const WithStaticProps = ({ items }: Props) => (
-  <Layout title="Users List | Next.js + TypeScript Example">
+  <>
     <h1>Users List</h1>
     <p>
       Example fetching data from inside <code>getStaticProps()</code>.
@@ -23,13 +22,10 @@ const WithStaticProps = ({ items }: Props) => (
         <a>Go home</a>
       </Link>
     </p>
-  </Layout>
+  </>
 )
 
 export const getStaticProps: GetStaticProps = async () => {
-  // Example for including static props in a Next.js function component page.
-  // Don't forget to include the respective types for any props passed into
-  // the component.
   const items: User[] = sampleUserData
   return { props: { items } }
 }
